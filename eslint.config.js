ac6bf3e2825c3f021e756d5d655605973b78abdb
Christina Frezynski <christina@frez.me>
2025-07-29 11:03:57 -0500

    init commit vite, mui, eslint, prettier

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import perfectionist from 'eslint-plugin-perfectionist'
+
+export default tseslint.config(
+    { ignores: ['dist', 'src/client/*'] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            perfectionist,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            'react-refresh/only-export-components': 'off',
+
+            // Perfectionist recommended-natural configuration
+            ...perfectionist.configs['recommended-natural'].rules,
+            'perfectionist/sort-enums': 'off',
+
+            // Due to having the exact same types between evaluation types
+            '@typescript-eslint/no-namespace': 'off',
+        },
+    },
+    // Configuration for files that should ignore perfectionist rules
+    {
+        files: ['**/constants.ts'],
+        rules: {
+            'perfectionist/sort-objects': 'off',
+            'perfectionist/sort-array-elements': 'off',
+            'perfectionist/sort-class-members': 'off',
+            'perfectionist/sort-named-exports': 'off',
+            'perfectionist/sort-named-imports': 'off',
+        },
+    },
+)
